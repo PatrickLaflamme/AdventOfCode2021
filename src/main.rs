77b47b3a080de28bd9366f265fbe0f338ee4a2cf
patricklaflamme <patrickj.laflamme@gmail.com>
2021-12-04 09:03:54 -0500

    setup deps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate pretty_env_logger;
+#[macro_use] extern crate log;
+
 use aoc_runner_derive::aoc_main;
 
-aoc_main! { lib = advent_of_code_2018 }
+aoc_main! { lib = advent_of_code_2021 }
