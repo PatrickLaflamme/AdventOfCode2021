c0787b93c9f2f20f848ce2694c0bb027e10b06a2
patricklaflamme <patrickj.laflamme@gmail.com>
2021-12-07 07:56:53 -0500

    optmize solution further

--- src/solutions/day7.rs
+++ src/solutions/day7.rs
@@ -1,35 +1,42 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 use itertools::sorted;
+use num::signum;
 use std::cmp::min;
 
 #[aoc_generator(day7)]
-pub fn generator(raw_input: &str) -> Vec<usize> {
+pub fn generator(raw_input: &str) -> Vec<isize> {
     raw_input.split(",")
-        .map(|s| s.parse::<usize>().unwrap())
+        .map(|s| s.parse::<isize>().unwrap())
         .collect()
 }
 
 #[aoc(day7, part1)]
-pub fn solve_part1(positions: &[usize]) -> usize {
+pub fn solve_part1(positions: &[isize]) -> isize {
     let n = positions.len();
     let sorted_positions = sorted(positions);
     let med = (*sorted_positions[(n as f64 / 2.0).ceil() as usize] as f64 + *sorted_positions[(n as f64 / 2.0).floor() as usize] as f64) / 2.0;
     positions.iter()
         .map(|&x| x as f64)
-        .fold(0.0, |dist, x| dist + (med - x).abs()) as usize
+        .fold(0.0, |dist, x| dist + (med - x).abs()) as isize
 }
  
 #[aoc(day7, part2)]
-pub fn solve_part2(positions: &[usize]) -> usize {
-    let mut min_cost = usize::MAX;
-    for i in 0..*positions.iter().max().unwrap() {
+pub fn solve_part2(positions: &[isize]) -> isize {
+    let mut min_cost = isize::MAX;
+    let sorted_positions = sorted(positions);
+    let mut i = positions.iter().sum::<isize>() / positions.len() as isize;
+    let direction = signum((sorted_positions[sorted_positions.len() - 1]- i) - (i - sorted_positions[0]));
+    loop {
         let cost = positions.iter()
-            .map(|&x| x as isize)
             .fold(0, |cost, x| {
-                let abs_diff = (i as isize - x).abs();
+                let abs_diff = (i - x).abs();
                 cost + (abs_diff * (abs_diff + 1)) / 2
-            }) as usize;
-        min_cost = min(cost, min_cost)
+            }) as isize;
+        if min_cost != isize::MAX && min_cost < cost {
+            break;
+        } 
+        i = i + direction;
+        min_cost = min(cost, min_cost);
     }
     return min_cost;
 }
@@ -48,13 +55,13 @@ mod tests {
 
     #[test]
     fn test_solve_part1() {
-        let example: Vec<usize> = generator(&EXAMPLE);
+        let example: Vec<isize> = generator(&EXAMPLE);
         assert_eq!(solve_part1(&example), 37);
     }
 
     #[test]
     fn test_solve_part2() {
-        let example: Vec<usize> = generator(&EXAMPLE);
+        let example: Vec<isize> = generator(&EXAMPLE);
         assert_eq!(solve_part2(&example), 168);
     }
 }
